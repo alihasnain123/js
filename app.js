32d246f0c67cb10fb62e2639d4c8edd214298cd3
alihasnain123 <alikk320@yahoo.com>
2021-08-03 23:32:14 +0500

    assignment

---
+++ app.js
@@ -0,0 +1,313 @@
+// Assgnment chapter no 1
+
+// // Question 1
+// alert("Hello world")
+
+// // Question 2
+// alert("Error! Please enter a valid password")
+
+// // Question 3
+
+// alert("Welcome to js land \n Happy Coding")
+
+// // Question 4
+
+// alert("Welcome to js lands")
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs ")
+
+// Question 5
+//   var a = alert("Hello... I can run JS through my web browser's console");
+// console.log(a)
+
+// // Question 6
+
+// function alertfunction(){
+
+// 	alert("alert using in html and css")
+// }
+
+	
+// Assgnment chapter no 1 end
+
+
+// Assgnment chapter no 2 start
+  // // Question 1
+//  var    username = "abc";
+
+// // Question 2
+//  var my_name = "xyz";
+
+
+// // Question 3
+
+//  var msg = "hello world";
+
+//  alert(msg);
+
+// // Question 4
+//  var name = prompt("enter your name : ");
+//  var age = prompt("enter your age : ");
+//  var course = prompt("enter your course : ");
+
+//  alert(name);
+  
+//  alert(age);
+//  alert(course);
+  
+// // Question 5
+//  var p = "pizza";
+
+//  alert(p.substr(0,5)+"\n"+p.substr(0,4)+"\n"+
+//  p.substr(0,3)+"\n"+p.substr(0,2)+"\n"+p.substr(0,1)+"\n");
+
+// // Question 6
+// var email = "abc@gmail.com";
+
+// alert("My email address is "+email)
+   
+
+// // Question 7
+// var book = "A smarter way to learn JavaScript";
+
+// alert("I am trying to learn from the book "+book)
+
+// // // Question 8
+// document.write("<p> Yah i can write HTML content through javascritp </p>")
+
+// // // Question 9
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// Assgnment chapter no 2 end
+
+// Assgnment chapter no 3 start
+
+// // // Question 1
+//  var age = 22;
+
+//   alert("I am \t" + age + "\t years old")
+
+//  // Question 2
+
+// var visted = 5;
+
+// alert("You have visited this site this \t" +visted+ "times")
+
+// // Queston 3
+
+
+//   var birthYear = 1999;
+
+//   document.write("My Birth Year is \t" + birthYear)
+//   document.write("<br>Data type of my decleard variable is number <br>")
+
+
+// // Queston 4
+
+//  var name = prompt("Enter your name")
+//  var product = prompt("Enter your Product")
+//  var quantity = prompt("Enter your Quantity")
+
+// document.write(name +"\n\n\norder\n\n"+ quantity+"\n\n"+product+"(s) on XYZ Clothing store")
+
+// Assgnment chapter no 3 end
+
+
+// Assignment Chapter 5 Start 
+
+// // Question 3
+// var a = 
+
+// document.write("value after declaration:" + a);
+
+
+// var b = 5
+
+// document.write("<br>intial value is : \n\n" + b);
+// document.write("<br>Value after increment is:\n\n " + ++b);
+
+// var c = 7
+// var d = (c+b)
+
+// document.write("<br>value after addition is:\n\n" + d);
+// document.write("<br>Value after decrement is:\n\n " + --d);
+
+// var e =(d%3)
+
+// document.write("<br>The remainder  is:\n\n " + e);
+
+
+// // Question 4
+
+// var f = 600
+// var g = (f*5)
+
+// document.write("Total coast to buy 5 tickets to a movie is\n\n " + g+"PKR");
+
+// Question 5
+
+// var table = 9;
+// var length = 10;
+// var i = 1;
+
+// document.write("Multiplication table: "+ table);
+// for(i = 1; i <= length; i++)
+//  document.write("<br>"+table+" * "+i+" = " +(table * i )); 
+
+ 
+// // Question 6 
+
+//  var cel = 34
+//  var far = 65
+// var cn = (far-32)*5/9
+// var fn = (cel * 5/9)+32
+
+
+// document.write(cel+"\n<sup>0</sup>C"+"\n\nto\n\n"+fn+"\n<sup>0</sup>F")
+// document.write("<br>"+far+"\n<sup>0</sup>F"+"\n\nto\n\n"+cn+"\n<sup>0</sup>C")
+
+
+// // Question 7
+
+//  var i1 = 650
+//  var i2 = 100
+//  var q1 = 3
+//  var q2 = 7
+//  var sp = 100
+//  var total = (i1*q1)+(i2*q2)+sp
+
+//  document.write("<h1>Shopping Cart</h1><br><br><br><br>");
+
+//  document.write("Price of item 1 :\n\n" + i1);
+//  document.write("<br>Ordered quantity of item 1 :\n\n" + q1);
+//  document.write("<br>Price of item 2 :\n\n" + i2);
+//  document.write("<br>Ordered quantity of item 2 :\n\n" + q2);
+// document.write("<br>Shipping charges :\n\n" + sp+"<br><br><br><br>");
+
+
+// document.write("Total coast of your order is :\n\n" + total)
+
+
+// // Question  8
+
+// var tm = +prompt("Enter your Total Marks")
+// var om = +prompt("Enter your Obtain Marks")
+// var tmarks = (om/tm)*100
+
+// document.write("<h1>Marksheet</h1><br><br><br><br>");
+// document.write("Total Marks :\n\n" + tm);
+// document.write("<br>Obtain Marks:\n\n" + om);
+// document.write("<br>Percentage :\n\n" + tmarks);
+
+// //  Question 9
+
+// var cr = (10*104.80)+(25*28)
+
+// document.write("Total currency in PKR:\n\n\n" + cr);
+
+// // Question 10
+
+//   var x = 10;
+//   var a = (x + 10) * (x / 2) + 100;
+
+// console.log(a)
+
+// // Question 11 
+
+  // var presentyear = 2021
+  // var birthyear = +prompt("Enter your Birth Year")
+
+  // var age = (presentyear-birthyear)
+  //   document.write("<h1>Age Calculator</h1><br>");
+  // document.write("Current Year : \n\n" + presentyear);
+  // document.write("<br> Birth year : \n\n" + birthyear);
+  // document.write("<br> Your Age : \n\n" + age);
+
+
+// //  Question 12
+
+// var radius = +prompt("Enter Radius of Circle")
+// var Circumference = (2*3.14*radius)
+// var area = (3.14*radius*radius)
+
+
+// document.write("<h1>The Geometrizer</h1><br>");
+// document.write("Radius of a Circle :\n\n" + radius);
+// document.write("<br> Circumference is :\n\n" + Circumference);
+// document.write("<br> Area is :\n\n" + area)
+
+// // Question 13
+// var Fvrt = prompt("Enter Your Favourate Snack");
+// var age = +prompt("Enter your age");
+// var maxAge = 80;
+// var amount = +prompt("Amount of snack per day");
+// var totalNeeded = (amount * 365) * (maxAge - age);
+
+// document.write("<h1>The lifetime Supply Calculator  </h1><br>");
+// document.write("<br>Favourate Snack :\n\n" + Fvrt);
+// document.write("<br>Current age :\n\n" + age);
+// document.write("<br>Estimated Maximum Age :\n\n" + maxAge);
+// document.write("<br>Amount of  Snack per day :\n\n" + amount);
+// document.write("<br>You will need\n" + totalNeeded +"\n\n"+Fvrt +"\n to last you until the ripe old age of\n"+ maxAge);
+
+
+// Assignment Chapter 6 to 9 Start
+ 
+//  // Questiuon 1
+// var number = +prompt("Enter a Number");
+
+
+// document.write("Result:<br>The Value of a is\n\n" + number + "<br>____________________________");
+// document.write("<br>The Value of ++a is\n\n" + ++number +"<br>now the value is\n\n"+number);
+// document.write("<br><br><br>The Value of a++ is\n\n" + (number++) +"<br>now the value is\n\n"+number);
+// document.write("<br><br><br>The Value of --a is\n\n" + (--number) +"<br>now the value is\n\n"+number);
+// document.write("<br><br><br>The Value of a-- is\n\n" + (number--) +"<br>now the value is\n\n"+number);
+
+// // Question 3
+
+//  var username = prompt("Enter Your name");
+
+//  document.write("Welcome to our website :\n\n" + username);
+
+// Question 5
+ //  var table = +prompt("Enter Number", "0") || 5;
+ //  var length = 10;
+ //  var i = 1;
+   
+ //  document.write("Multiplication table: "+ table);
+ // for(i = 1; i <= length; i++)
+ //   document.write("<br>"+table+" * "+i+" = " +(table * i ));
+
+// // Question 6
+
+// var Subject1 = prompt("Enter First subject ");
+// var Subject2 = prompt("Enter second subject ");
+// var Subject3 = prompt("Enter third subject ");
+// var totalmarks = 100;
+// var obtain1 = +prompt("Enter First subject Marks ");
+// var obtain2 = +prompt("Enter second subject Marks");
+// var obtain3 = +prompt("Enter Third subject Marks");
+// var sumtotal = (totalmarks+totalmarks+totalmarks);
+// var sumobtain = (obtain1+obtain2+obtain3);
+// document.write("<table><tr><td><b>Subject</b></td>\n\n<td><b>Total Marks</b></td>\n\n<td><b>Obtain marks</b></td>\n\n<td><b>Precentage</b></td></tr><br><tr><td>"+Subject1+"</td>\n\n<td>"+totalmarks+"</td>\n\n<td>"+obtain1+"</b></td>\n\n<td>"+((obtain1/totalmarks)*100)+"</td></tr><br><tr><td>"+Subject2+"</td>\n\n<td>"+totalmarks+"</td>\n\n<td>"+obtain2+"</b></td>\n\n<td>"+((obtain2/totalmarks)*100)+"</td></tr><br><tr><td>"+Subject3+"</td>\n\n<td>"+totalmarks+"</td>\n\n<td>"+obtain3+"</b></td>\n\n<td>"+((obtain3/totalmarks)*100)+"</td></tr><br><tr><td></td>\n\n<td>"+sumtotal+"</td>\n\n<td>"+sumobtain+"</b></td>\n\n<td>"+(sumobtain/sumtotal*100)+"</td></tr></table>");
+
+
+// Assigment Chapter 9 to 11
+// Question 1
+var city = prompt("Select city Karachi,Hyderabad,lahore");
+
+ if(city === "Karachi" ){
+
+     document.write("Welcome to the city of lights");
+ }else if(city === "hyderabad"){
+
+
+  document.write("Welcome to the city of sweets");
+
+
+ }else if (city === "lahore") {
+
+document.write("Welcome to the city of gardens");   
+ }else {
+   document.write("Enter the correct city");
+ }
+
